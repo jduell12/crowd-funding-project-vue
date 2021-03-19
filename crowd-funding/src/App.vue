7e0e6d70f3f31da66c196b141ee2fe6d67f25e5c
Jessica <duellje@oregonstate.edu>
2021-03-18 21:27:39 -0400

    reset css with myers reset

--- crowd-funding/src/App.vue
+++ crowd-funding/src/App.vue
@@ -16,7 +16,16 @@ export default {
 </script>
 
 <style>
-body{
+@import url('https://fonts.googleapis.com/css2?family=Commissioner:wght@400;500;600&display=swap');
+* {
+  box-sizing: border-box;
+}
+
+html, body{
+  height: 100%;
+  padding: 0;
+  margin: 0;
   text-align: center;
+  font-family: 'Commissioner', sans-serif;
 }
 </style>
