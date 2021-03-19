eed578db2283a609d04c7885415c61e12a2c0610
Jessica <duellje@oregonstate.edu>
2021-03-18 21:47:36 -0400

    added styling to match design

--- crowd-funding/src/App.vue
+++ crowd-funding/src/App.vue
@@ -5,27 +5,29 @@
 </template>
 
 <script>
-import Header from './components/Header'
+import Header from "./components/Header";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header
   }
-  
-}
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Commissioner:wght@400;500;600&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Commissioner:wght@400;500;600&display=swap");
 * {
   box-sizing: border-box;
 }
 
-html, body{
+html,
+body {
   height: 100%;
   padding: 0;
   margin: 0;
   text-align: center;
-  font-family: 'Commissioner', sans-serif;
+  font-family: "Commissioner", sans-serif;
+  background: url("../images/image-hero-desktop.jpg") no-repeat fixed;
+  background-size: 100%;
 }
 </style>
--- crowd-funding/src/components/Header.vue
+++ crowd-funding/src/components/Header.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-        <h3>Header</h3>
-    </div>
+  <header>
+    <img src="../../images/logo.svg" alt="logo" />
+
+    <nav>
+      <a href="">About</a>
+      <a href="">Discover</a>
+      <a href="">Get Started</a>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "Header"
-}
+  name: "Header"
+};
 </script>
 
 <style scoped>
-
+header {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 3%;
+}
+img {
+  margin-right: 45%;
+}
+nav {
+  width: 20%;
+  display: flex;
+  justify-content: space-between;
+  margin-right: 10%;
+}
+a {
+  color: white;
+  text-decoration: none;
+}
 </style>
